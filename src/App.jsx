66720bf6b3a621e1755784df8344e5f6423bff5f
Wasif Tai <muhammadwasiftai@gmail.com>
2026-09-17 10:21:59 +0500

    [UPDATE] Learned Use State and Set Value

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import StudentCard from "./StudentCard";
 import studentOne from "./assets/student 01.png";
@@ -6,11 +7,43 @@ import studentThree from "./assets/student 03.png";
 import studentFour from "./assets/student 04.png";
 
 function App() {
+  // useState is a hook
+  // Hook is a function that can be used in functional component
+  // useState return array, in the 0 index there is value and in index 1 there is function to update that value
+  // when value is state updated, react rerenders the HTML and updated value is shown in the UI.
+  let [value, setValue] = useState();
+  console.log("value", value);
+  console.log("setValue", setValue);
+
   const students = [
-    { name: "Ava Morgan", className: "8-A", grade: "A+", color: "blue", image: studentOne },
-    { name: "Noah Williams", className: "8-B", grade: "A", color: "green", image: studentTwo },
-    { name: "Mia Anderson", className: "7-A", grade: "B+", color: "purple", image: studentThree },
-    { name: "Leo Thompson", className: "7-B", grade: "A-", color: "orange", image: studentFour },
+    {
+      name: "Sara Khan",
+      className: "8-A",
+      grade: "A+",
+      color: "blue",
+      image: studentOne,
+    },
+    {
+      name: "Ali Ahmed",
+      className: "8-B",
+      grade: "A",
+      color: "green",
+      image: studentTwo,
+    },
+    {
+      name: "Mia Anderson",
+      className: "7-A",
+      grade: "B+",
+      color: "purple",
+      image: studentThree,
+    },
+    {
+      name: "Leo Thompson",
+      className: "7-B",
+      grade: "A-",
+      color: "orange",
+      image: studentFour,
+    },
   ];
 
   return (
